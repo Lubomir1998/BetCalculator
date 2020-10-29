92e313b8e2763630486aeed3eb1f1a6fb734cc73
Lubomir1998 <lubcho_sk8@abv.bg>
2020-10-29 18:56:06 +0200

    show how to divide the bet between the 2 coefs by coloring it

--- app/src/main/java/com/example/betcalculator/MainActivity.kt
+++ app/src/main/java/com/example/betcalculator/MainActivity.kt
@@ -1,10 +1,20 @@
 package com.example.betcalculator
 
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
+import androidx.core.graphics.toColorInt
 import com.example.betcalculator.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 import java.math.RoundingMode
@@ -15,6 +25,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -29,17 +40,49 @@ class MainActivity : AppCompatActivity() {
                 val koef2 = binding.koef2EditText.text.toString().toFloat()
                 val bet = binding.betEditText.text.toString().toFloat()
 
-
                 val x = (koef1 * bet) / (koef1 + koef2)
                 val totalSum = (koef2 * x).toBigDecimal().setScale(2, RoundingMode.FLOOR).toFloat()
                 val neto = (totalSum - bet).toBigDecimal().setScale(2, RoundingMode.FLOOR).toFloat()
                 val profit = ((neto / bet) * 100).toBigDecimal().setScale(2, RoundingMode.FLOOR).toFloat()
 
-                binding.resultTextView.text =
-                        "Profit: ${profit}% \n" +
-                        "[1] ${koef1} x ${(bet-x).toBigDecimal().setScale(2, RoundingMode.FLOOR).toFloat()} = $totalSum \n" +
-                        "[2] ${koef2} x ${x.toBigDecimal().setScale(2, RoundingMode.FLOOR).toFloat()} = $totalSum \n" +
-                        "Neto: $neto"
+                val sum1 = (bet-x).toBigDecimal().setScale(2, RoundingMode.FLOOR).toFloat()
+                val sum2 = x.toBigDecimal().setScale(2, RoundingMode.FLOOR).toFloat()
+
+                val builder = SpannableStringBuilder()
+
+//                val resultString =
+//                        "Profit: ${profit}% \n" +
+//                        "[1] $koef1 x $sum1 = $totalSum \n" +
+//                        "[2] $koef2 x $sum2 = $totalSum \n" +
+//                        "Neto: $neto"
+
+                val result1 = "Profit: ${profit}% \n[1] $koef1 x "
+                val spannableString = SpannableString(result1)
+                builder.append(spannableString)
+
+                val colorSpan = ForegroundColorSpan(Color.BLUE)
+                val colorSpan2 = ForegroundColorSpan(Color.BLUE)
+
+                val result2 = "$sum1"
+                val spannableString2 = SpannableString(result2)
+                spannableString2.setSpan(colorSpan, 0, result2.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+                builder.append(spannableString2)
+
+                val result3 = " = $totalSum \n[2] $koef2 x "
+                val spannableString3 = SpannableString(result3)
+                builder.append(spannableString3)
+
+                val result4 = "$sum2"
+                val spannableString4 = SpannableString(result4)
+                spannableString4.setSpan(colorSpan2, 0, result4.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+                builder.append(spannableString4)
+
+                val result5 = " = $totalSum \nNeto: $neto"
+                val spannableString5 = SpannableString(result5)
+                builder.append(spannableString5)
+
+                binding.resultTextView.setText(builder, TextView.BufferType.SPANNABLE)
+
             }
             else{
                 Snackbar.make(binding.constraintLayout, "All fileds must be filled!", Snackbar.LENGTH_SHORT).show()
